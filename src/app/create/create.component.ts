1ee51018f4a59405bfc38e8b707507804510b4d4
Pushkar Krishna Karakala <32445721+pushkarreddy2305@users.noreply.github.com>
2019-08-23 13:54:17 -0500

    Default Checkbox checked

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,63 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms'
-import { ProjectService } from '../services/project.service'
-import { Router } from '@angular/router';
-import { environment } from './../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { IEntity } from './entity/project.entity';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ProjectService } from "../services/project.service";
+import { Router } from "@angular/router";
+import { environment } from "./../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { IEntity } from "./entity/project.entity";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.scss"]
 })
-
 export class CreateComponent implements OnInit {
-  public selectedMenu = 'Select Template'
-  
+  public selectedMenu = "Select Template";
+  public checked = true;
+
+  togleCheck() {
+    this.checked = !this.checked;
+  }
   data = new IEntity();
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     public project: ProjectService,
     private router: Router,
     private http: HttpClient
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   projectForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    description: ['', [Validators.required, Validators.maxLength(100)]],
+    name: ["", Validators.required],
+    description: ["", [Validators.required, Validators.maxLength(100)]],
     //projectTitle: ['', [Validators.required, Validators.maxLength(100)]],
-    key: ['', [Validators.required, Validators.maxLength(100)]],
-    refreshprotect : []
-   //. pageContent: ['', [Validators.required, Validators.maxLength(100)]]
+    key: ["", [Validators.required, Validators.maxLength(100)]],
+    refreshprotect: []
+    //. pageContent: ['', [Validators.required, Validators.maxLength(100)]]
   });
 
   create() {
     let project = {
-      details: this.projectForm.value,
-    }
+      details: this.projectForm.value
+    };
     project.details.components = [];
     console.log(project.details);
-    this.http.post(environment.backend.url + "/project", project.details)
-    .subscribe(
-      (res) => {
+    this.http
+      .post(environment.backend.url + "/project", project.details)
+      .subscribe(res => {
         console.log(res);
-        this.router.navigateByUrl('project').then(nav => {
-          console.log(nav);
-        }, err => {
-          console.log(err);
-        });
-      })
-
+        this.router.navigateByUrl("project").then(
+          nav => {
+            console.log(nav);
+          },
+          err => {
+            console.log(err);
+          }
+        );
+      });
   }
 
   cancel() {
-    this.router.navigate(['/project'])
+    this.router.navigate(["/project"]);
   }
 
   selectTemplate(templateName) {
-    this.selectedMenu = templateName
+    this.selectedMenu = templateName;
   }
 }
